2f0f8e611886466b1d9ee65554d4f7c0df90c86e
Comayocode <ronalc726@gmail.com>
2023-10-19 21:53:15 -0500

    add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { useState } from 'react';
+import '../stylesheets/Navbar.css';
+
+import logo from '../assets/logo.svg';
+import close from '../assets/Close.svg';
+import menu from '../assets/Menu.svg';
+// const imgs = require.context('');
+import { navLinks, socialMedia } from '../constants';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  return (
+    <nav className='main-menu'>
+      <div className='logo-container'>
+        <img src={logo} alt='Logo optica mejía' className='main-menu__logo' />
+        <p className='main-menu__logo-text'>Óptica Mejía</p>
+      </div>
+      <ul className='main-menu__list hidden'>
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`main-menu__item ${
+              index === navLinks.length - 1
+                ? 'main-menu__item--margin-bottom-0'
+                : 'main-menu__item--margin-bottom-10'
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='social-media'>
+        {socialMedia.map((social, index) => (
+          <a
+            key={social.id}
+            href={social.link}
+            rel='noreferrer'
+            target='_blank'
+            className='social-media__link'
+          >
+            <img
+              src={social.icon}
+              alt={social.id}
+              className={`social-media__img ${
+                index === socialMedia.length - 1
+                  ? 'main-menu__item--margin-right-0'
+                  : 'main-menu__item--margin-right-10'
+              }`}
+            />
+          </a>
+        ))}
+      </div>
+
+      <div className='mobile-menu'>
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='mobile-menu__img'
+          onClick={() => setToggle((state) => !state)}
+        />
+        <div className={`${toggle ? 'flex' : 'hidden'} mobile-menu__card`}>
+          <ul className='main-menu__list main-menu__list--mobile'>
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`main-menu__item main-menu__item--mobile ${
+                  index === navLinks.length - 1
+                    ? 'main-menu__item--margin-right-0'
+                    : 'main-menu__item--margin-right-10'
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
