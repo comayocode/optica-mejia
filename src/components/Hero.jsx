41cf36d358b4a7bee8bf6685e0b3cae7304dd8e9
Comayocode <ronalc726@gmail.com>
2023-10-20 20:37:46 -0500

    feat: add hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import '../stylesheets/Hero.css';
+import { heroImg, heroTextLine } from '../assets';
+import { hero } from '../constants';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+  <section id='home'>
+    <div className='red-space'>
+      <img src={heroImg} alt='Hero image' className='hero-img' />
+    </div>
+    <div className='hero-info'>
+      <h1 className='hero-info__title'>{hero.tittle}</h1>
+      <img src={heroTextLine} alt='Line decoration' className='hero-info__line-decoration'/>
+      <p className='hero-info__description'>{hero.description}</p>
+      <button className='hero-info__btn' href='https://www.youtube.com/'>
+        Contacta con nosotros
+      </button>
+    </div>
+  </section>
+);
 
-export default Hero
+export default Hero;
