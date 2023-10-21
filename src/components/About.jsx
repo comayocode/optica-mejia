80328272ec5c4a653a48fc59b3a3639f598803c9
Comayocode <ronalc726@gmail.com>
2023-10-21 03:33:14 -0500

    feat: add about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { about } from '../constants';
+import '../stylesheets/About.css';
 
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+const About = () => (
+  <section id='about' className='about'>
+    <h2 className='about__title'>Sobre Nosotros</h2>
+    {about.map((value) => (
+      <div key={value.id} className='about-content'>
+        <div className='img-container'>
+          <img
+            src={value.img}
+            alt='Sobre optica mejia'
+            className='about__img'
+          />
+        </div>
+        <div className='about-description-container'>
+          <p className='about__description'>{value.text1}</p>
+          <br />
+          <p className='about__description'>{value.text2}</p>
+        </div>
+      </div>
+    ))}
+  </section>
+);
 
-export default About
+export default About;
