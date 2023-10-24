df53d3d111a1954e43509753071abb0a595be5b9
Comayocode <ronalc726@gmail.com>
2023-10-24 15:28:39 -0500

    refactor: update gallery image size

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { products } from '../constants';
+import '../stylesheets/Products.css';
 
-const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+const Products = () => (
+  <section id='products' className='products'>
+    {products.map((product, index) => (
+      <div
+        key={product.id}
+        className={`product-item ${'product-item' + '-' + (index + 1)}`}
+      >
+        <img src={product.img} alt='Gafas' className='product-item__img'></img>
+      </div>
+    ))}
+  </section>
+);
 
-export default Products
+export default Products;
