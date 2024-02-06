f9e9ebcab0b004808cda79f59d1e4c49c4b8019c
Comayocode <ronalc726@gmail.com>
2024-02-06 15:10:10 -0500

    add new logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
 import { logo } from "../assets";
 import '../stylesheets/Logo.css'
 
-const Logo = () => (
+const Logo = ({click, isOpen}) => (
   /* Componente del logo de la aplicación */
-  <div className='logo-container'>
+  <div className='logo-container' onClick={click}>
     <img src={logo} alt='Logo optica mejía' className='main-menu__logo' />
-    <p className='main-menu__logo-text'>Óptica Mejía</p>
+    <p className='main-menu__logo-text' style={{display: isOpen ? "block" : "none"}}>OPTICA MEJIA</p>
   </div>
 );
 
